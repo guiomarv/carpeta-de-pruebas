b4ca60d2c8db330aeb7e546a5e981c9b81fee44d
“guiomarv <gvaldes@prisatv.com>
2015-09-30 16:42:21 +0200

    otra actualización subida

---
+++ js-ejercicios/js/funciones.js
@@ -0,0 +1,16 @@
+
+
+
+//declaracióon de variables
+
+var miNombre;
+var tipoComida;
+var NumeroComida;
+var Frase;
+
+
+
+var Frase =miNombre + "comió" +NumeroComida +tipoComida;
+alert(Frase);
+
+
